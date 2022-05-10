d23e453da54a3d99d1aff08161d9127dfc066974
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2022-05-10 12:07:15 +1000

    Register page with api completed

--- front-end-expense-tracker/src/components/register/Register.js
+++ front-end-expense-tracker/src/components/register/Register.js
@@ -1,21 +1,70 @@
-import React from "react";
-import { Button, Form, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Alert, Button, Form, Row, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { postRegister } from "../../helper/axiosHelper";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+};
 
 export const Register = () => {
+  const [formDt, setFormDt] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
+  const [res, setRes] = useState({});
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormDt({ ...formDt, [name]: value });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    //call api using axios
+    const { data } = await postRegister(formDt);
+    console.log(data);
+    setRes(data);
+    setIsLoading(false);
+  };
+
   return (
     <Row className="login-comp mt-5">
-      <Form>
+      <Form onSubmit={handleOnSubmit}>
         <h3>Register to Join!!</h3>
         <hr />
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        {isLoading && (
+          <Spinner variant="danger text-center" animation="border" />
+        )}
+
+        {res.message && (
+          <Alert variant={res.status === "success" ? "success" : "danger"}>
+            {res.message}
+          </Alert>
+        )}
+
+        <Form.Group className="mb-3" controlId="formBasicFullName">
           <Form.Label>Full Name</Form.Label>
-          <Form.Control type="email" placeholder="Your Full Name" />
+          <Form.Control
+            onChange={handleOnChange}
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            onChange={handleOnChange}
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -23,7 +72,13 @@ export const Register = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            onChange={handleOnChange}
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
 
         <Button variant="success" type="submit">
